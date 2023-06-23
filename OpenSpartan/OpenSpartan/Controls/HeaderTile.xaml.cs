e13fe93f4bf41127a2fda14a464341f14696a393
Den Delimarsky <1389609+dend@users.noreply.github.com>
2023-06-23 10:49:55 -0700

    Add support for header tiles

---
+++ OpenSpartan/OpenSpartan/Controls/HeaderTile.xaml.cs
@@ -0,0 +1,87 @@
+using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using System.Numerics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace OpenSpartan.Controls
+{
+    public sealed partial class HeaderTile : UserControl
+    {
+        Compositor _compositor = Microsoft.UI.Xaml.Media.CompositionTarget.GetCompositorForCurrentThread();
+        private SpringVector3NaturalMotionAnimation _springAnimation;
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(HeaderTile), new PropertyMetadata(null));
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(HeaderTile), new PropertyMetadata(null));
+
+        public object Source
+        {
+            get { return (object)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(object), typeof(HeaderTile), new PropertyMetadata(null));
+
+        public string Link
+        {
+            get { return (string)GetValue(LinkProperty); }
+            set { SetValue(LinkProperty, value); }
+        }
+
+        public static readonly DependencyProperty LinkProperty =
+            DependencyProperty.Register("Link", typeof(string), typeof(HeaderTile), new PropertyMetadata(null));
+
+
+        public HeaderTile()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Element_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            CreateOrUpdateSpringAnimation(1.1f);
+            (sender as UIElement).CenterPoint = new Vector3(70, 40, 1f);
+            (sender as UIElement).StartAnimation(_springAnimation);
+        }
+
+        private void Element_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            CreateOrUpdateSpringAnimation(1.0f);
+            (sender as UIElement).CenterPoint = new Vector3(70, 40, 1f);
+            (sender as UIElement).StartAnimation(_springAnimation);
+        }
+
+        private void CreateOrUpdateSpringAnimation(float finalValue)
+        {
+            if (_springAnimation == null)
+            {
+                if (_compositor != null)
+                {
+                    _springAnimation = _compositor.CreateSpringVector3Animation();
+                    _springAnimation.Target = "Scale";
+                }
+            }
+
+            _springAnimation.FinalValue = new Vector3(finalValue);
+        }
+    }
+}
--- OpenSpartan/OpenSpartan/Views/HomeView.xaml.cs
+++ OpenSpartan/OpenSpartan/Views/HomeView.xaml.cs
@@ -27,5 +27,32 @@ namespace OpenSpartan.Views
         {
             this.InitializeComponent();
         }
+
+        private void scroller_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateScrollButtonsVisibility();
+        }
+
+        private void UpdateScrollButtonsVisibility()
+        {
+            if (scroller.ScrollableWidth > 0)
+            {
+                ScrollForwardBtn.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ScrollForwardBtn.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void ScrollBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            scroller.ChangeView(scroller.HorizontalOffset - scroller.ViewportWidth, null, null);
+        }
+
+        private void ScrollForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            scroller.ChangeView(scroller.HorizontalOffset + scroller.ViewportWidth, null, null);
+        }
     }
 }
