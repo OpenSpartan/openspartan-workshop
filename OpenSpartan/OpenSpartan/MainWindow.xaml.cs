c2683d69e4393d432de511b2f39d82d7984c145f
Den Delimarsky <1389609+dend@users.noreply.github.com>
2023-06-23 15:28:30 -0700

    Remove unnecessary namespaces

--- OpenSpartan/OpenSpartan/MainWindow.xaml.cs
+++ OpenSpartan/OpenSpartan/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-using Microsoft.Identity.Client;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
-using System.Linq;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
