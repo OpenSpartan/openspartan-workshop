864d73ea45f4e5ec18cb005be3fa8f308a2c56da
Den Delimarsky <1389609+dend@users.noreply.github.com>
2023-06-27 23:23:33 -0700

    Move some things around

--- OpenSpartan/OpenSpartan/App.xaml.cs
+++ OpenSpartan/OpenSpartan/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.Extensions.Msal;
 using Microsoft.UI.Xaml;
+using OpenSpartan.Shared;
 using System.Diagnostics;
 using System.Linq;
 
@@ -32,41 +33,8 @@ namespace OpenSpartan
             m_window = new MainWindow();
             m_window.Activate();
 
-            InitializePublicClientApplication();
-        }
-
-        private async void InitializePublicClientApplication()
-        {
-            var storageProperties = new StorageCreationPropertiesBuilder(Authentication.Configuration.CacheFileName, Authentication.Configuration.CacheDirectory).Build();
-
-            var pca = PublicClientApplicationBuilder.Create(Authentication.Configuration.ClientID).WithAuthority(AadAuthorityAudience.PersonalMicrosoftAccount).Build();
-
-            // This hooks up the cross-platform cache into MSAL
-            var cacheHelper = await MsalCacheHelper.CreateAsync(storageProperties);
-            cacheHelper.RegisterCache(pca.UserTokenCache);
-
-            IAccount accountToLogin = (await pca.GetAccountsAsync()).FirstOrDefault();
-
-            AuthenticationResult authResult = null;
-
-            try
-            {
-                authResult = await pca.AcquireTokenSilent(Authentication.Configuration.Scopes, accountToLogin)
-                                            .ExecuteAsync();
-            }
-            catch (MsalUiRequiredException)
-            {
-                authResult = await pca.AcquireTokenInteractive(Authentication.Configuration.Scopes)
-                                            .WithAccount(accountToLogin)
-                                            .ExecuteAsync();
-            }
-
-            if (authResult != null)
-            {
-                // Authentication succeeded.
-                Debug.WriteLine("Authenticated.");
-            }
-        }
+            AuthenticationManager.InitializePublicClientApplication();
+        }        
 
         private Window m_window;
     }
