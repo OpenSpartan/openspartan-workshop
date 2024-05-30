710f004790374bb77158bd3f136cb9f85561fa00
Den Delimarsky <sign@den.dev>
2024-05-30 14:48:20 -0700

    Fixes #25

---
+++ src/OpenSpartan.Workshop/Converters/ListCountToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace OpenSpartan.Workshop.Converters
+{
+    internal class ListCountToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is IEnumerable enumerable)
+            {
+                return enumerable.Cast<object>().Any() ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
