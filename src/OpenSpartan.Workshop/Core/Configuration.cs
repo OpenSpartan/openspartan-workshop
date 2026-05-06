614e8bf45d3f26a6e91e1940ca1fe0dd144defc0
Den Delimarsky <1389609+dend@users.noreply.github.com>
2026-05-06 16:57:46 -0700

    Update default API layout

--- src/OpenSpartan.Workshop/Core/Configuration.cs
+++ src/OpenSpartan.Workshop/Core/Configuration.cs
@@ -23,9 +23,9 @@ namespace OpenSpartan.Workshop.Core
         internal static readonly string AppDataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), PackageName);
 
         // API-related default metadata.
-        internal const string DefaultRelease = "1.8";
+        internal const string DefaultRelease = "1.13";
         internal const string DefaultAPIVersion = "1";
-        internal const string DefaultHeaderImage = "progression/Switcher/Season_Switcher_S8_Champions.png";
+        internal const string DefaultHeaderImage = "progression/Switcher/Season_Switcher_S13_Infinite.png";
         internal const string DefaultSandbox = "UNUSED";
         internal const string DefaultBuild = "259234.24.07.25.1725-2";
 
